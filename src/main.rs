d6ebcb6a8656b7bfcfd4c98badc540e614c549d3
Sean McArthur <sean.monstar@gmail.com>
2015-04-21 13:21:40 -0700

    add Expires and Pragma headers

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 extern crate hyper;
 extern crate rustc_serialize;
 extern crate env_logger;
+extern crate time;
 
 use std::env;
 use std::io::{self, Read, Write};
 use std::str;
 
 use rustc_serialize::json;
+use time::Tm;
 
 fn main() {
     env_logger::init().unwrap();
@@ -28,6 +30,20 @@ fn get_port() -> u16 {
 
 static INDEX_PAGE: &'static [u8] = include_bytes!("../assets/index.html");
 
+const JAN_1990: Tm = Tm {
+    tm_sec: 0,
+    tm_min: 0,
+    tm_hour: 0,
+    tm_mday: 1,
+    tm_mon: 0,
+    tm_year: 90,
+    tm_wday: 0,
+    tm_yday: 0,
+    tm_isdst: 0,
+    tm_utcoff: 0,
+    tm_nsec: 0,
+};
+
 fn handle(req: hyper::server::Request, mut res: hyper::server::Response<hyper::net::Fresh>) {
     match (req.method, req.uri) {
         (hyper::Get, hyper::uri::RequestUri::AbsolutePath(ref path)) if path == "/" => {
@@ -46,6 +62,10 @@ fn handle(req: hyper::server::Request, mut res: hyper::server::Response<hyper::n
             let badge = format!("https://img.shields.io/badge/crates.io-{}-green.svg", version);
             *res.status_mut() = hyper::status::StatusCode::Found;
             res.headers_mut().set(hyper::header::Location(badge));
+            res.headers_mut().set(hyper::header::Expires(
+                hyper::header::HttpDate(JAN_1990)
+            ));
+            res.headers_mut().set(hyper::header::Pragma::NoCache);
             res.headers_mut().set(hyper::header::CacheControl(vec![
                 hyper::header::CacheDirective::NoCache,
                 hyper::header::CacheDirective::NoStore,
